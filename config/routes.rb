aa5a439590eeea3f380ec208ee7bfd8e7140c5c3
Mathilde Couraud <mcouraud@outlook.fr>
2019-02-13 12:21:26 +0100

    models restaurant et restaurant_review + restaurant controller done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'restaurant/index'
+  get 'restaurant/show'
+  get 'restaurant/new'
+  get 'restaurant/edit'
+  get 'restaurant/create'
+  get 'restaurant/update'
+  get 'restaurant/destroy'
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20190213105133_create_restaurants.rb
@@ -0,0 +1,15 @@
+class CreateRestaurants < ActiveRecord::Migration[5.2]
+  def change
+    create_table :restaurants do |t|
+      t.string :name
+      t.text :description
+      t.string :photo
+      t.string :address
+      t.string :phone
+      t.string :price
+      t.text :menu
+
+      t.timestamps
+    end
+  end
+end
