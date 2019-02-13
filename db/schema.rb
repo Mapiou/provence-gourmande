344e73bbacecdaf3ec75915d9b9b7d6c0748dfe5
Mathilde Couraud <mcouraud@outlook.fr>
2019-02-13 18:38:57 +0100

    geocoder et search done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_13_105252) do
+ActiveRecord::Schema.define(version: 2019_02_13_135821) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 2019_02_13_105252) do
     t.text "menu"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
   add_foreign_key "restaurant_reviews", "restaurants"
